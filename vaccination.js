ff4ff1cec101624087bf05b1937f5bd68492d7ec
sitrucp <sitrucp@users.noreply.github.com>
2021-01-13 23:18:47 -0500

    first working iteration

--- vaccination.js
+++ vaccination.js
@@ -36,13 +36,9 @@ Promise.all([
     
     document.getElementById('title').innerHTML += ' <small class="text-muted">Last updated: ' + lastUpdated + '</small>';
 
-    // ggt case and mortality totals by summing values
+    // ggt dist and admin totals by summing values
     var distTotalCanada = dist_canada.reduce((a, b) => +a + +b.dvaccine, 0);
     var adminTotalCanada = admin_canada.reduce((a, b) => +a + +b.avaccine, 0);
-
-    // get canada dist & admin max dates
-    maxDistDate = d3.max(dist_canada.map(d=>d.report_date));
-    maxAdminDate = d3.max(admin_canada.map(d=>d.report_date));
 
     //==== population data start ====
     // filter population by age_group
@@ -52,10 +48,10 @@ Promise.all([
     });
     
     // summarize population by Canada
-    var popByCanada = populationFiltered.reduce((a, b) => +a + +b.population, 0);
+    var popCanada = populationFiltered.reduce((a, b) => +a + +b.population, 0);
 
     // summarize population by province
-    var popByProv = d3.nest()
+    var popProv = d3.nest()
         .key(function(d) { return d.geo; })
         .rollup(function(v) { return {
             population: d3.sum(v, function(d) { return d.population; })
@@ -82,7 +78,7 @@ Promise.all([
     dist_canada.forEach(function(d) {
         d.report_date = reformatDate(d.date_vaccine_distributed)
         d.prov_date = d.province + '|' + d.date_vaccine_distributed
-        d.population = popByCanada
+        d.population = popCanada
     });
     admin_canada.forEach(function(d) {
         d.report_date = reformatDate(d.date_vaccine_administered)
@@ -113,7 +109,7 @@ Promise.all([
         {avaccine:"0", cumulative_avaccine:"0"});
 
     // map population to distAdminProv
-    const distAdminProvPop = distAdminProv.map(t1 => ({...t1, ...popByProv.find(t2 => t2.province === t1.province)}))
+    const distAdminProvPop = distAdminProv.map(t1 => ({...t1, ...popProv.find(t2 => t2.province === t1.province)}))
 
     // add percentages to distAdminProvPop
     distAdminProvPop.forEach(function(d) {
@@ -125,49 +121,142 @@ Promise.all([
     // create charts
     // call createCharts when page loads, or when user changes age filter
 
+    // get canada dist & admin max dates
+    var maxDistDate = d3.max(dist_canada.map(d=>d.report_date));
+    var maxAdminDate = d3.max(admin_canada.map(d=>d.report_date));
+
+    function createFutureData(pop, maxDate, dist, admin, prov) {
+        // forecast distribution are here: 
+        // https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection/prevention-risks/covid-19-vaccine-treatment/vaccine-rollout.html
+
+        // calculate daysRemaining (# days) eg maxDate to Sep 30
+        var datex = maxDate.split('-');
+        var startDate = new Date(datex[1] + '/' + datex[2] + '/' + datex[0]);
+        var endDate = new Date("9/30/2021");
+        var daysRemaining = Math.floor((endDate - startDate) / (1000*60*60*24))
+
+        // create future data
+        var futureData = [];
+        var province = prov;
+        var avaccine = ((pop * 2) - (admin)) / daysRemaining;
+        var dvaccine = ((pop * 2) - (dist)) / daysRemaining;
+
+        for (var i=1; i<daysRemaining; i++) {
+            var report_date = new Date(startDate);
+            report_date.setDate(report_date.getDate() + i);
+            var cumulative_avaccine = admin + (avaccine * i);
+            var cumulative_dvaccine = dist + (dvaccine * i);
+            var pct_dist_admin = cumulative_avaccine / cumulative_dvaccine;
+            var pct_pop_admin = cumulative_avaccine / pop;
+            var pct_pop_dist = cumulative_dvaccine / pop;
+
+            futureData.push({
+                province, 
+                report_date, 
+                avaccine, 
+                dvaccine, 
+                cumulative_avaccine, 
+                cumulative_dvaccine, 
+                pct_dist_admin,
+                pct_pop_admin,
+                pct_pop_dist
+            });
+        }
+        return futureData;
+    }
+
     function createCanadaChart() {
+
+        // ggt dist and admin totals by summing values, and population using max
+        var province = "Canada";
+        var max_pct_dist_admin = d3.max(distAdminCanadaPop.map(d=>d.pct_dist_admin));
+        var population = d3.max(distAdminCanadaPop.map(d=>d.population));
+        var max_pct_dist_admin = d3.max(distAdminCanadaPop.map(d=>d.pct_dist_admin));
+
+        // get future data
+        var futureData = createFutureData(popCanada, maxAdminDate, distTotalCanada, adminTotalCanada, province);
 
         // CREATE CANADA CHART
 
+        // concat actual and future data
+        var dataConcat = distAdminCanadaPop.concat(futureData);
+
         // create x and y axis data sets
         var x = [];
-        var yDistAdmin = [];
-        var yPopAdmin = [];
+        var yDist = [];
+        var yAdmin = [];
         var yPopDist = [];
+        var yPopAdmin = [];
+        var yDistAdmin = [];
 
         // create axes x and y arrays
-        for (var i=0; i<distAdminCanadaPop.length; i++) {
-            var row = distAdminCanadaPop[i];
+        for (var i=0; i<dataConcat.length; i++) {
+            var row = dataConcat[i];
             x.push(row['report_date']);
-            yDistAdmin.push(row['pct_dist_admin']);
-            yPopAdmin.push(row['pct_pop_admin']);
+            yDist.push(parseInt(row['dvaccine']));
+            yAdmin.push(parseInt(row['avaccine']));
             yPopDist.push(row['pct_pop_dist']);
+            yPopAdmin.push(row['pct_pop_admin']);
+            yDistAdmin.push(row['pct_dist_admin']);
         }
 
         var pctDistAdmin = {
             name: 'pctDistAdmin',
             x: x,
             y: yDistAdmin,
-            type: 'scatter',
+            type: 'bar',
         };
         
         var pctPopAdmin = {
             name: 'pctPopAdmin',
             x: x,
             y: yPopAdmin,
-            type: 'scatter'
+            type: 'bar'
         };
 
         var pctDistAdmin = {
             name: 'pctPopDist',
             x: x,
             y: yPopDist,
-            type: 'scatter'
+            type: 'bar'
         };
         
-        var data = [pctDistAdmin, pctPopAdmin];
-        
-        Plotly.newPlot('divCanadaChart', data);
+        var dist7DMA = {
+            name: 'admin7DMA',
+            x: x,
+            y: movingAverage(yDist, 10),
+            type: 'scatter'
+        };
+
+        var adminDaily = {
+            name: 'adminDaily',
+            x: x,
+            y: yAdmin,
+            type: 'bar'
+        };
+
+        var admin7DMA = {
+            name: 'admin7DMA',
+            x: x,
+            y: movingAverage(yAdmin, 7),
+            type: 'scatter'
+        };
+
+        // create divs, para for Canada chart
+        var canadaDiv = 'canadaDiv';
+        var canadaTitle = 'title' + canadaDiv;
+        var titleCanadaChart = document.createElement("p");
+        var divCanadaChartItem = document.createElement("div");
+        divCanadaChartItem.id = canadaDiv;
+        titleCanadaChart.id = canadaTitle;
+        var chartDetails = '<ul class="list-unstyled"><li><h1>Canada</h1></li><li>Popluation: ' + population + '</li><li>Received: ' + distTotalCanada + '</li><li>Administered: ' + adminTotalCanada + '</li><li>% Administered: ' + max_pct_dist_admin + '</li></ul>';
+
+        titleCanadaChart.innerHTML  = chartDetails;
+        document.getElementById('divCanadaChart').append(titleCanadaChart);
+        document.getElementById('divCanadaChart').append(divCanadaChartItem);
+
+        var data = [adminDaily];
+        Plotly.newPlot('canadaDiv', data);
 
     }
 
@@ -175,33 +264,51 @@ Promise.all([
         // CREATE PROV CHART
 
         // get list of provinces 
-        provArray = [];
+        provListTemp = [];
         for (var i=0; i<distAdminProvPop.length; i++) {
-            provArray.push(distAdminProvPop[i]['province']);
+            province = distAdminProvPop[i]['province'];
+            provListTemp.push(province);
         }
-        let provList = [...new Set(provArray)];
+        let provList = [...new Set(provListTemp)];
+        console.log(provList);
 
-        // loop through provList to create chart for each prov
+        // create prov charts by loop through provList to create chart for each prov
         for (var i=0; i<provList.length; i++) {
-            
-            console.log(provList[i], i);
 
             var provData = distAdminProvPop.filter(function(d) { 
                 return d.province === provList[i];
             });
 
+            // ggt dist and admin totals by summing values, and population using max
+            var distTotalProv = provData.reduce((a, b) => +a + +b.dvaccine, 0);
+            var adminTotalProv = provData.reduce((a, b) => +a + +b.avaccine, 0);
+            var population = d3.max(provData.map(d=>d.population));
+            var max_pct_dist_admin = d3.max(provData.map(d=>d.pct_dist_admin));
+
+            // get future data 
+            var futureData = createFutureData(population, maxAdminDate, distTotalProv, adminTotalProv, provList[i]);
+
+            // concat actual and future data
+            var dataConcat = provData.concat(futureData);
+
+            // CREATE PROV CHART
             // create x and y axis data sets
             var x = [];
-            var yDistAdmin = [];
-            var yPopAdmin = [];
+            var yDist = [];
+            var yAdmin = [];
             var yPopDist = [];
+            var yPopAdmin = [];
+            var yDistAdmin = [];
 
-            for (var j=0; j<provData.length; j++) {
-                var row = provData[j];
+            // create axes x and y arrays
+            for (var j=0; j<dataConcat.length; j++) {
+                var row = dataConcat[j];
                 x.push(row['report_date']);
-                yDistAdmin.push(row['pct_dist_admin']);
-                yPopAdmin.push(row['pct_pop_admin']);
+                yDist.push(parseInt(row['dvaccine']));
+                yAdmin.push(parseInt(row['avaccine']));
                 yPopDist.push(row['pct_pop_dist']);
+                yPopAdmin.push(row['pct_pop_admin']);
+                yDistAdmin.push(row['pct_dist_admin']);
             }
 
             // create Prov chart
@@ -209,38 +316,60 @@ Promise.all([
                 name: 'pctDistAdmin',
                 x: x,
                 y: yDistAdmin,
-                type: 'scatter',
+                type: 'bar',
             };
             
             var pctPopAdmin = {
                 name: 'pctPopAdmin',
                 x: x,
                 y: yPopAdmin,
-                type: 'scatter'
+                type: 'bar'
             };
-            
+    
             var pctDistAdmin = {
                 name: 'pctPopDist',
                 x: x,
                 y: yPopDist,
-                type: 'scatter'
+                type: 'bar'
             };
             
-            //var data = [pctDistAdmin, pctPopAdmin, pctPopDist];
-            var data = [pctPopAdmin, pctDistAdmin];
+            var dist7DMA = {
+                name: 'admin7DMA',
+                x: x,
+                y: movingAverage(yDist, 10),
+                type: 'scatter'
+            };
+    
+            var adminDaily = {
+                name: 'adminDaily',
+                x: x,
+                y: yAdmin,
+                type: 'bar'
+            };
+    
+            var admin7DMA = {
+                name: 'admin7DMA',
+                x: x,
+                y: movingAverage(yAdmin, 7),
+                type: 'scatter'
+            };
 
+            // create divs, para for each province chart
             var provDiv = 'provDiv' + i;
             var provTitle = 'title' + provDiv;
             var titleProvChart = document.createElement("p");
             var divProvChartItem = document.createElement("div");
-            
             divProvChartItem.id = provDiv;
             titleProvChart.id = provTitle;
-            titleProvChart.textContent = provList[i];
+            var chartDetails = '<ul class="list-unstyled"><li><h1>' + provList[i] + '</h1></li><li>Popluation: ' + population + '</li><li>Received: ' + distTotalProv + '</li><li>Administered: ' + adminTotalProv + '</li><li>% Administered: ' + max_pct_dist_admin + '</li></ul>';
 
+            titleProvChart.innerHTML  = chartDetails;
             document.getElementById('divProvChart').append(titleProvChart);
             document.getElementById('divProvChart').append(divProvChartItem);
+            
+            var data = [adminDaily];
             Plotly.newPlot(provDiv, data);
+
         }
     }
 
